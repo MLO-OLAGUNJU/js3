ea1090968d9b8d628ea1465f279945036de3e7f4
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-15 01:45:03 +0100

    learning javascript

--- js/main.js
+++ js/main.js
@@ -3,4 +3,14 @@
 const myVariable = "Mathematics";
 
 //The length property
-console.log(myVariable.length);
+// console.log("Every good boy does fine".length);
+
+// string methods
+// console.log(myVariable.charAt(6));
+// console.log(myVariable.indexOf("ati"));
+// console.log(myVariable.lastIndexOf("ath"));
+// console.log(myVariable.slice(0, 8));
+// console.log(myVariable.toUpperCase());
+// console.log(myVariable.toLowerCase());
+// console.log(myVariable.includes("mat"));
+console.log("john joe dace jgjg".split(" "));
